22d2a1088b12da43f337270a5cb315488bc6f642
David Lin <davidzylin@outlook.com>
2025-03-29 18:39:30 -0400

    Update network_summary.py

--- agents/src/agents/network_summary.py
+++ agents/src/agents/network_summary.py
@@ -6,7 +6,7 @@ from agents.mcp import MCPServerStdio
 async def main():
     async with MCPServerStdio(
         cache_tools_list=True,  # Cache the tools list, for demonstration
-        params={"command": "/root/.local/bin/uv", "args": ["--directory", "/root/onos-mcp-server/src/onos_mcp_server", "run", "server.py"]},
+        params={"command": "/root/.local/bin/uv", "args": ["--directory", "/root/onos-mcp-server/src/onos_mcp_server", "run", "server.py"], "env": {"ONOS_API_BASE": "onos"}},
     ) as onos_mcp_server:
         agent = Agent(name="Assistant", instructions="You are a helpful assistant", mcp_servers=[onos_mcp_server])
 
